5716b4132ae864eab1cabc97df7ffe78ba4cdcea
Adam Coulter <ads@me.com>
2022-02-24 17:40:01 +1100

    Add GetModuleAsync to ModuleRegistry

--- src/ModuleRegistry.cs
+++ src/ModuleRegistry.cs
@@ -7,44 +7,35 @@ using System.Text.Json;
 namespace TF;
 public class ModuleRegistry
 {
+	public JwtSecurityToken Token { get; set; }
+	public Uri Endpoint { get; set; }
 	public ModuleRegistry(Uri endpoint, string jwtToken)
 	{
 		Endpoint = endpoint;
 		Token = new JwtSecurityToken(jwtToken);
 	}
 
-	public JwtSecurityToken Token { get; set; }
-	public Uri Endpoint { get; set; }
+	public async Task<Module> GetModuleAsync(ModuleReference moduleReference)
+	{
+		var response = await httpRequest(HttpMethod.Get, $"v1/modules/{moduleReference.Path}");
+		var moduleJson = await response.Content.ReadAsStringAsync();
+		var module = JsonSerializer.Deserialize<Module>(moduleJson);
+		if (module is null)
+			throw new ModuleNotFoundException(moduleReference);
+		return module;
+	}
 
 	public async Task DownloadModuleAsync(DirectoryInfo path, ModuleReference moduleReference)
 	{
-		using var client = new HttpClient();
+		moduleReference.Version ??= (await GetModuleAsync(moduleReference)).LatestVersion;
 
-		var urlBuilder = new UriBuilder(Endpoint)
-		{
-			Scheme = "https",
-			Path = $"v1/modules/{moduleReference.Path}"
-		};
-		var request = new HttpRequestMessage(HttpMethod.Get, urlBuilder.Uri);
-		var authHeader = new AuthenticationHeaderValue("Bearer", Token.RawData);
-		request.Headers.Authorization = authHeader;
-		var response = await client.SendAsync(request);
-		var moduleJson = await response.Content.ReadAsStringAsync();
-		var module = JsonSerializer.Deserialize<Module>(moduleJson);
-		if(module is null)
-			throw new ModuleNotFoundException(moduleReference);
-		moduleReference.Version ??= module.LatestVersion;
-
-		urlBuilder.Path += "/download";
-		request = new HttpRequestMessage(HttpMethod.Get, urlBuilder.Uri);
-		request.Headers.Authorization = authHeader;
-		response = await client.SendAsync(request);
+		var response = await httpRequest(HttpMethod.Get, $"v1/modules/{moduleReference.Path}/download");
 		var urlString = response.Headers.GetValues("X-Terraform-Get").First();
 		if (response.StatusCode != HttpStatusCode.NoContent || urlString is null)
 			throw new ModuleNotFoundException(moduleReference);
+
 		var urlDownload = new Uri(urlString);
-		var downloadRequest = new HttpRequestMessage(HttpMethod.Get, urlDownload);
-		var download = await client.SendAsync(downloadRequest, HttpCompletionOption.ResponseHeadersRead);
+		var download = await httpRequest(HttpMethod.Get, urlDownload, true);
 		var filePath = new FileInfo($"{path.FullName}/{moduleReference.FullName}.zip");
 		var writeStream = File.OpenWrite(filePath.FullName);
 		var readStream = await download.Content.ReadAsStreamAsync();
@@ -53,5 +44,18 @@ public class ModuleRegistry
 		readStream.Close();
 		ZipFile.ExtractToDirectory(filePath.FullName, filePath.DirectoryName!);
 		filePath.Delete();
+	}
+
+	private AuthenticationHeaderValue _authHeader => new AuthenticationHeaderValue("Bearer", Token.RawData);
+	private async Task<HttpResponseMessage> httpRequest(HttpMethod method, string path, bool justHeaders = false)
+		=> await httpRequest(method, new UriBuilder(Endpoint) { Path = path }.Uri, justHeaders);
+	private async Task<HttpResponseMessage> httpRequest(HttpMethod method, Uri uri, bool justHeaders = false)
+	{
+		using var client = new HttpClient();
+		var request = new HttpRequestMessage(method, uri);
+		request.Headers.Authorization = _authHeader;
+		var completionOption = justHeaders ? HttpCompletionOption.ResponseHeadersRead : HttpCompletionOption.ResponseContentRead;
+		var response = await client.SendAsync(request, completionOption);
+		return response;
 	}
 }
