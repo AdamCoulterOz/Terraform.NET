b1ee24006d9935f308f9976699fec870177b002f
Adam Coulter <ads@me.com>
2023-08-21 22:15:32 +1000

    use primary constructors

--- src/Modules/ModuleNotFoundException.cs
+++ src/Modules/ModuleNotFoundException.cs
@@ -1,10 +1,7 @@
 namespace TF;
 
-public class ModuleNotFoundException : Exception
+public class ModuleNotFoundException(ModuleReference moduleReference)
+	: Exception($"Module not found: {moduleReference.Path}")
 {
-	public ModuleReference ModuleReference { get; set; }
-
-	public ModuleNotFoundException(ModuleReference moduleReference)
-		: base($"Module not found: {moduleReference.Path}")
-		=> ModuleReference = moduleReference;
+    public ModuleReference ModuleReference { get; set; } = moduleReference;
 }
